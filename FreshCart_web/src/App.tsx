839a4e885935365f85e19210d4dba159ef489419
saswati <saswatirath43@gmail.com>
2026-08-18 13:01:10 +0530

    Created Login component

--- FreshCart_web/src/App.tsx
+++ FreshCart_web/src/App.tsx
@@ -1,16 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Hero from "./components/Hero";
 import Login from "./pages/Login";
+import Body from "./pages/Body";
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/" element={<Hero />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
--- FreshCart_web/src/pages/Login.tsx
+++ FreshCart_web/src/pages/Login.tsx
@@ -1,84 +1,35 @@
 const Login = () => {
   return (
-    <div className="min-h-full flex justify-center items-center ">
-      <div className="flex flex-col justify-center px-8 py-5 lg:px-10 w-max rounded-lg shadow-lg shadow-gray-200">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-2 text-center text-2xl/9 font-bold tracking-tight text-orange-600">
-            Sign in to your account
-          </h2>
-        </div>
+    <div className="hero bg-base-200">
+      <div className="hero-content w-full max-w-sm flex-col">
+        <h2 className="text-center text-2xl/9 font-bold tracking-tight">
+          Sign in to your account
+        </h2>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm/6 font-medium text-gray-600"
-              >
-                Email
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  placeholder="Enter your email address"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
+        <div className="card bg-base-100 w-full max-w-3xl shrink-0 shadow-2xl">
+          <div className="card-body">
+            <fieldset className="fieldset w-full">
+              <label className="label">Email</label>
+              <input
+                type="email"
+                className="input w-full"
+                placeholder="Email"
+              />
+
+              <label className="label">Password</label>
+              <input
+                type="password"
+                className="input w-full"
+                placeholder="Password"
+              />
+
+              <div>
+                <a className="link link-hover">Forgot password?</a>
               </div>
-            </div>
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm/6 font-medium text-gray-600"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  placeholder="Enter your password"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm/6 text-gray-500">
-            Don't have an account?{" "}
-            <a
-              href="#"
-              className="font-semibold text-orange-600 hover:text-indigo-500"
-            >
-              Sign up now
-            </a>
-          </p>
+              <button className="btn btn-neutral mt-4 w-full">Login</button>
+            </fieldset>
+          </div>
         </div>
       </div>
     </div>
